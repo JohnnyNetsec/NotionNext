5d0a6d404d2e5aeefcea0f95fb63551e4508f0f7
Johnny Netsec <116501930+JohnnyNetsec@users.noreply.github.com>
2026-06-13 16:55:42 -0400

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -9,7 +9,7 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: 'Welcome to NetSec Effective Thinking Notes Site!', url: 'https://blog.51sec.org' },
-    { title: 'Check Shares ', url: 'https://alist.51sec.org/' }
+    { title: 'Check Shares ', url: 'https://alist.51sec.org/' },
     { title: 'Get more sites ...', url: 'https://sites.51sec.org' }
   ],
 
